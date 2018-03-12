fe6c142f1394a37d970514201dc8a403eb3a0deb
Sandra Kuzmic <sandra.kuzmic@fer.hr>
2018-03-12 13:02:35 +0100

    add selectable list

--- app/src/main/java/android/sandra/com/starwarsapp/HomeScreenActivity.kt
+++ app/src/main/java/android/sandra/com/starwarsapp/HomeScreenActivity.kt
@@ -2,11 +2,24 @@ package android.sandra.com.starwarsapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class HomeScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        val listView = findViewById<ListView>(R.id.lv_home_screen)
+        val swCategories = resources.getStringArray(R.array.sw_categories)
+
+        swCategories.forEach { Log.d("TAG", it) }
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item,
+                android.R.id.text1, swCategories)
+
+        listView.adapter = adapter
     }
 }
