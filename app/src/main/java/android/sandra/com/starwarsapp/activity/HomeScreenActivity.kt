cee356a21143548aeb36a707d56e6e94513c59f9
Sandra Kuzmic <sandra.kuzmic@fer.hr>
2018-03-12 13:10:07 +0100

    refactor activity

--- app/src/main/java/android/sandra/com/starwarsapp/activity/HomeScreenActivity.kt
+++ app/src/main/java/android/sandra/com/starwarsapp/activity/HomeScreenActivity.kt
@@ -1,8 +1,8 @@
-package android.sandra.com.starwarsapp
+package android.sandra.com.starwarsapp.activity
 
 import android.os.Bundle
+import android.sandra.com.starwarsapp.R
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -15,10 +15,11 @@ class HomeScreenActivity : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.lv_home_screen)
         val swCategories = resources.getStringArray(R.array.sw_categories)
 
-        swCategories.forEach { Log.d("TAG", it) }
-
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item,
-                android.R.id.text1, swCategories)
+        val adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_selectable_list_item,
+                android.R.id.text1,
+                swCategories)
 
         listView.adapter = adapter
     }
