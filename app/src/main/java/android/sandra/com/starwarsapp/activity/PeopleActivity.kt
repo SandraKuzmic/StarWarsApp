1a0d19e9dcd671cf1952607fb7cf8018b1510a3c
Sandra Kuzmic <sandra.kuzmic@fer.hr>
2018-03-15 13:41:41 +0100

    finish first version of people activity

--- app/src/main/java/android/sandra/com/starwarsapp/activity/PeopleActivity.kt
+++ app/src/main/java/android/sandra/com/starwarsapp/activity/PeopleActivity.kt
@@ -1,13 +1,36 @@
 package android.sandra.com.starwarsapp.activity
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.sandra.com.starwarsapp.PEOPLE_BUNDLE_KEY
 import android.sandra.com.starwarsapp.R
+import android.sandra.com.starwarsapp.databinding.ActivityPeopleBinding
+import android.sandra.com.starwarsapp.entity.Person
 import android.support.v7.app.AppCompatActivity
 
 class PeopleActivity : AppCompatActivity() {
 
+    private var mBinding: ActivityPeopleBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_people)
+
+        val person = intent.getSerializableExtra(PEOPLE_BUNDLE_KEY) as Person
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_people)
+
+        populateActivityLayout(person)
+    }
+
+    private fun populateActivityLayout(person: Person) {
+        mBinding?.tvPersonName?.text = person.name
+        mBinding?.tvPersonGender?.append(person.gender)
+        mBinding?.tvPersonBirthYear?.append(person.birthYear)
+        mBinding?.tvPersonHeight?.text = resources.getString(R.string.height_cm, person.height)
+        mBinding?.tvPersonMass?.text = resources.getString(R.string.mass_kg, person.mass)
+        mBinding?.tvPersonSkinColor?.append(person.skinColor)
+        mBinding?.tvPersonHairColor?.append(person.hairColor)
+        mBinding?.tvPersonEyeColor?.append(person.eyeColor)
     }
 }
