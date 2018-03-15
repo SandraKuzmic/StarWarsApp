6abc4bbfc681517d800ee0b32c3c837288cfa075
Sandra Kuzmic <sandra.kuzmic@fer.hr>
2018-03-15 17:24:44 +0100

    add opening planet activity from species

--- app/src/main/java/sandra/com/starwarsapp/activity/SpeciesActivity.kt
+++ app/src/main/java/sandra/com/starwarsapp/activity/SpeciesActivity.kt
@@ -1,5 +1,6 @@
 package sandra.com.starwarsapp.activity
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.sandra.com.starwarsapp.R
@@ -11,6 +12,7 @@ import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import sandra.com.starwarsapp.PLANET_BUNDLE_KEY
 import sandra.com.starwarsapp.SPECIES_BUNDLE_KEY
 import sandra.com.starwarsapp.entity.Planet
 import sandra.com.starwarsapp.entity.Species
@@ -34,7 +36,7 @@ class SpeciesActivity : AppCompatActivity() {
 
         var planetOfSpecies: Planet
 
-        disposal = swapiService.getPlanet(species.homeworld.takeLast(3)[1].toInt())
+        disposal = swapiService.getPlanet(getPlanetId(species.homeworld))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
@@ -67,7 +69,16 @@ class SpeciesActivity : AppCompatActivity() {
         mBinding.tvSpeciesSkinColors.append(species.skinColors)
         mBinding.tvSpeciesHairColors.append(species.hairColors)
         mBinding.tvSpeciesEyeColors.append(species.eyeColors)
+
+        mBinding.tvSpeciesHomeworld.setOnClickListener { _ ->
+            val intent = Intent(this, PlanetsActivity::class.java)
+            intent.putExtra(PLANET_BUNDLE_KEY, planet)
+
+            startActivity(intent)
+        }
     }
+
+    private fun getPlanetId(url: String): Int = url.takeLast(3)[1].toInt()
 
     private fun visibilityProgressBar(pbLoad: ProgressBar, visible: Boolean) {
         pbLoad.visibility = if (visible) View.VISIBLE else View.GONE
